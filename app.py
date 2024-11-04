882551a24deb5652d311d3bbfc01b154375772ae
rajayarli <rajarameshy1@gmail.com>
2024-11-05 02:52:09 +0530

    Bind Flask app to Heroku's dynamic port

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request, jsonify
 from LoanCalculator import LoanCalculator
 from datetime import datetime, timedelta
@@ -79,4 +80,5 @@ def calculate():
     })
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Heroku provides $PORT
+    app.run(host='0.0.0.0', port=port, debug=True)
